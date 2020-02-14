ab55480cd594b4789f90fa1fbb3a4cd19e8c723d
Doodle173 <52260791+Doodle173@users.noreply.github.com>
2020-02-14 12:37:45 -0500

    almost functional

---
+++ CPPBulletHellGame/Game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+enum GameState {
+	GAME_ACTIVE,
+	GAME_MENU,
+	GAME_WIN
+};
+
+class Game{
+public:
+	//gamestate
+	GameState State;
+	GLboolean Keys[1024];
+	GLuint Width, Height;
+
+	Game(GLuint width, GLuint height);
+	~Game();
+
+	//initialize game state (load textures/shaders/levels
+	void Init();
+
+	//gameloop
+	void ProcessInput(GLfloat dt);
+	void Update(GLfloat dt);
+	void Render();
+
+};
+
+#endif
